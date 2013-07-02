c1217c4e1e665f1881c4ce078ff5a34e7be5ef56
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-07-02 09:32:17 -0700

    Change how Marionette.Layout calls its super constructor
    By using prototype.constructor you allow for overriding the
    Marionette.View constructor and have the new constructor be called from
    all extended views.

--- src/marionette.layout.js
+++ src/marionette.layout.js
@@ -17,8 +17,8 @@ Marionette.Layout = Marionette.ItemView.extend({
 
     this._firstRender = true;
     this._initializeRegions(options);
-    
-    Marionette.ItemView.call(this, options);
+
+    Marionette.ItemView.prototype.constructor.call(this, options);
   },
 
   // Layout's render will use the existing region objects the
